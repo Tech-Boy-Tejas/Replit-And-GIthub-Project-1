ec5682a443bc6a69b74d5b4957e1ac95b4516b76
The Big Tejas <67586986+Tech-Boy-Tejas@users.noreply.github.com>
2022-04-08 20:09:54 +0530

    Create file1.py

---
+++ file1.py
@@ -0,0 +1,6 @@
+print("Welcome to Surface area of cylinder calculator")
+
+x = int(input("Please enter the radius of the cylinder : ")
+y = int(input("Please enter the height of the cylinder : ")
+
+print("The Total Surface Area of the cylinder is " + (2 * (22/7 * r * r * h)))
